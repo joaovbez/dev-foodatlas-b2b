317d7d67dadc8e4300edf4cf638aeef0f197f4fc
João Victor <102772212+jfotonv@users.noreply.github.com>
2025-01-14 14:23:00 -0300

    Corrigindo bug pro deploy vercel ESLint

--- app/(user)/conta/users/list-users/data-table.tsx
+++ app/(user)/conta/users/list-users/data-table.tsx
@@ -26,6 +26,7 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table"
+import {ChevronDown, MoreHorizontal } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkboxUser"
