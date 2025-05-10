b642602e18a2b60109f2b4f2d1c54f56c05a723a
CaioToshiyuki <129913649+CaioToshiyuki@users.noreply.github.com>
2025-05-10 14:21:39 -0300

    Corrigir variavel de ambiente errada

--- lib/google-cloud-storage.ts
+++ lib/google-cloud-storage.ts
@@ -2,4 +2,4 @@ import { Storage } from '@google-cloud/storage'
 
 const storage = new Storage()
 
-export const bucket = storage.bucket(process.env.GCS_BUCKET!) 
+export const bucket = storage.bucket(process.env.GOOGLE_CLOUD_DATASET_FINANCE!) 
