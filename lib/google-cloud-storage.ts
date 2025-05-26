8ed725b87961b3c453788892e6db04ad8a722281
João Victor <102772212+joaovbez@users.noreply.github.com>
2025-05-26 11:10:18 -0300

    Update google-cloud-storage.ts

--- lib/google-cloud-storage.ts
+++ lib/google-cloud-storage.ts
@@ -2,4 +2,4 @@ import { Storage } from '@google-cloud/storage'
 
 const storage = new Storage()
 
-export const bucket = storage.bucket(process.env.GOOGLE_CLOUD_DATASET_FINANCE!) 
+export const bucket = storage.bucket(process.env.GOOGLE_CLOUD_STORAGE_BUCKET!) 
